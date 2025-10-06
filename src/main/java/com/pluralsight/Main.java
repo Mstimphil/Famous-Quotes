4b003471fd076bf50a85136ce4b2a7b6aba8e8b8
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-06 11:48:45 -0400

    quotes comment

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,8 +4,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Pick a number from 1-10");
         Scanner myScanner = new Scanner(System.in);
         int Scanner = myScanner.nextInt();
-        int[] numbers = new int[11];
+        int[] numbers = new int[10];
         numbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        String[] Quotes = {
+                "The future belongs to those who believe in the beauty of their dreams.",
+                "It always seems impossible until it’s done.",
+                "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+                "Don’t watch the clock; do what it does. Keep going.",
+                "The best way to predict the future is to create it.",
+                "In the middle of difficulty lies opportunity.",
+                "Dream big. Start small. Act now.",
+                "Believe you can and you’re halfway there.",
+                "Your time is limited, so don’t waste it living someone else’s life.",
+                "The secret of getting ahead is getting started."
+
+
+
+
+
+
+        };
 }}
