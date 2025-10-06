726ac25f99556edb9440774ca914157d762a76f6
Mstimphil <mstimphil@my.yearupunited.org>
2025-10-06 11:26:13 -0400

    int  comment

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         Scanner myScanner = new Scanner(System.in);
         int Scanner = myScanner.nextInt();
-
-    }
-}
+        int[] numbers = new int[11];
+        numbers = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+}}
